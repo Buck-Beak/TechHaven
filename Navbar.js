c7bb3a2eaf73582ba829c95f3f50617abdb45d14
Nanda Krishna <147984605+frostyscupcake@users.noreply.github.com>
2024-03-29 10:09:54 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        
+        <div className="links">
+          <Link to="/">Home</Link>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
